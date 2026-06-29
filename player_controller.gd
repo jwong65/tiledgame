3f47897cbfc665b7ba1a0a88dc29abb9e085c89e
jwong65 <jason.wong6507@gmail.com>
2026-06-29 15:41:18 -0400

    Added initial project files from Godot

---
+++ player_controller.gd
@@ -0,0 +1,22 @@
+extends Node2D
+@export var speed = 200
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var velocity = Vector2.ZERO
+	
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+	
+	position += velocity.normalized() * speed * delta
